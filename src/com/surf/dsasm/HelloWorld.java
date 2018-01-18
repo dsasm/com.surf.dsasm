15635c8216903cd8fcea61ebf923f33595da17ba
wwm13zru <Dylan@Dylan-PC.home>
2018-01-18 23:12:29 +0000

    test

--- src/com/surf/dsasm/HelloWorld.java
+++ src/com/surf/dsasm/HelloWorld.java
@@ -2,6 +2,9 @@ package com.surf.dsasm;
 
 public class HelloWorld {
 	public static void main(String [] args) {
+		System.setProperty("apikey", "qqXLspNvSZDeQr3UMQFzifjXjxEVOUhxBRu5wBaxaJzQbjmHyKlSUwJxhBfHv7zb");
+		System.setProperty("secret", "erKGeikPr6mUexUKJlyLS1mqdS9Zvno9TP1sRm2Hsn3rKAvJpMslbTVhpIIU1VVk");
+
 		TraderManager tm = new TraderManager();
 		tm.tryout();
 	}
--- src/com/surf/dsasm/TraderManager.java
+++ src/com/surf/dsasm/TraderManager.java
@@ -6,9 +6,42 @@ import com.binance.api.client.domain.market.TickerStatistics;
 
 public class TraderManager {
 	public void tryout() {
-		BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance("API-KEY", "SECRET");
+		BinanceApiClientFactory factory = createNewFactory(System.getProperty("apikey"),
+				System.getProperty("secret"));
 		BinanceApiRestClient client = factory.newRestClient();
 		TickerStatistics tickerStatistics = client.get24HrPriceStatistics("TRXETH");
-		System.out.println(tickerStatistics.getPriceChangePercent());
+
+		System.out.println("getCloseTime: "+tickerStatistics.getCloseTime());
+		System.out.println("getOpenTime: "+tickerStatistics.getOpenTime());
+		System.out.println("------");
+		System.out.println("askPrice: "+tickerStatistics.getAskPrice());
+		System.out.println("bidPrice: "+tickerStatistics.getBidPrice());
+		System.out.println("getLastPrice: "+tickerStatistics.getLastPrice());
+		System.out.println("getLowPrice: "+tickerStatistics.getLowPrice());
+		System.out.println("getOpenPrice: "+tickerStatistics.getOpenPrice());
+		System.out.println("------");
+		System.out.println("priceChangePercentage: "+tickerStatistics.getPriceChangePercent());
+		System.out.println("getPriceChange: "+tickerStatistics.getPriceChange());
+
+		//ask price and last price are the same given there is something to be bought
+		/*
+		getCloseTime: 1516317064654
+		getOpenTime: 1516230664654
+		------
+		askPrice: 0.00008190
+		bidPrice: 0.00008181
+		getLastPrice: 0.00008190
+		getLowPrice: 0.00007119
+		getOpenPrice: 0.00007144
+		------
+		priceChangePercentage: 14.642
+		getPriceChange: 0.00001046
+		 */
+
+
+	}
+	
+	public BinanceApiClientFactory createNewFactory(String apiKey, String apiSecret) {
+		return BinanceApiClientFactory.newInstance(apiKey, apiSecret);
 	}
 }
